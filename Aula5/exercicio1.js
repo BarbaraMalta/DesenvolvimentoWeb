3863bf0d01fc5f9002ad224b2e56f7d546107f55
BARBARA MALTA MORAES <202402898892@alunos.ibmec.edu.br>
2024-09-16 10:25:56 -0300

    criando aula5

---
+++ Aula5/exercicio1.js
@@ -0,0 +1,15 @@
+console.log(2+2);
+console.log("casa"+2);
+console.log("casa"+"amarela");
+console.log("casa"+2);
+console.log("casa"+"2");
+console.log(1 + "2");
+console.log(1 + "2" == 12);
+
+const meuObjeto = {
+    nome : "Eduardo",
+    idade : 48
+  }
+  
+  console.log(meuObjeto.nome)
+  console.log(meuObjeto.idade)
