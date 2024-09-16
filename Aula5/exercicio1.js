104e5bf2971f0fe3c879b3cadfda45f82579c00e
BARBARA MALTA MORAES <202402898892@alunos.ibmec.edu.br>
2024-09-16 10:46:41 -0300

    uptating

--- Aula5/exercicio1.js
+++ Aula5/exercicio1.js
@@ -13,3 +13,21 @@ const meuObjeto = {
   
   console.log(meuObjeto.nome)
   console.log(meuObjeto.idade)
+  console.log(meuObjeto.endereco)
+  console.log(meuObjeto.numero)
+
+  function nomeDaFuncao (x, y) {
+    return x + y
+}
+const func2 = function (a, b) {
+    return a * b
+}
+const func3 = (entrada) => {
+    return entrada + ' é bonito'
+}
+const func4 = entrada => entrada + ' é bonito'
+
+const minha_lista = [25, 'casa', [34,12], 9.65]
+
+const funcaoComNome = elemento => console.log(elemento);
+minha_lista.forEach(funcaoComNome);
